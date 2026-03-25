91b1c2edd3ad2565780d4e24d1998dab04c7cf63
gagan <gaganjio14@gmail.com>
2026-03-25 19:05:01 +0530

    Merge two sorted linked list -> Recursive approach

--- src/LinkedList/MergeTwoSortedLists.java
+++ src/LinkedList/MergeTwoSortedLists.java
@@ -13,7 +13,20 @@ public class MergeTwoSortedLists {
             this.next = null;
         }
     }
-    static Node merge(Node head1, Node head2){
+
+    static Node mergeRecursive(Node head1, Node head2){
+        if(head1 == null) return head2;
+        if(head2 == null) return head1;
+
+        if(head1.data <= head2.data){
+            head1.next = mergeRecursive(head1.next, head2);
+            return head1;
+        } else {
+            head2.next = mergeRecursive(head1, head2.next);
+            return head2;
+        }
+    }
+    static Node mergeIterative(Node head1, Node head2){
         Node dummy = new Node(-1);
         Node curr = dummy;
         while(head1 != null && head2 != null){
@@ -69,7 +82,7 @@ public class MergeTwoSortedLists {
             }
         }
 
-        Node result = merge(head1, head2);
+        Node result = mergeIterative(head1, head2);
 
         Node curr = result;
         while(curr != null){
